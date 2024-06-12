7da2d97233c531fa2d68fca5cd7a180ebb46014f
dawsonTolbert <tolbert_dawson@yahoo.com>
2024-06-11 22:31:44 -0400

    Set Up Dummy Params

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -53,6 +53,11 @@ public:
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
+    using APVTS = juce::AudioProcessorValueTreeState;
+    static APVTS::ParameterLayout creatParameterLayout();
+
+    APVTS apvts{ *this, nullptr, "Parameters", creatParameterLayout() };
+
 private:
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleMBCompAudioProcessor)
